756bc01c1e5f3a84e8ae4f7e41e20f0d986dd57c
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-01 12:09:55 +0300

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Omer Cagri Sayir on 1.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
